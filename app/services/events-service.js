40266596556c33fa541817850006a448ec3ed49a
Philippe Ndiaye <phndiaye@gmail.com>
2023-02-04 19:10:25 +0100

    just ran npx ember-codemod-v1-to-v2

--- app/services/events-service.js
+++
@@ -1,1 +0,0 @@
-export { default } from '@upfluence/hyperevents/services/events-service';
--- types/global.d.ts
+++
@@ -1,6 +0,0 @@
-// Types for compiled templates
-declare module '@upfluence/hyperevents/templates/*' {
-  import { TemplateFactory } from 'htmlbars-inline-precompile';
-  const tmpl: TemplateFactory;
-  export default tmpl;
-}
