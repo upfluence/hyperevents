feb9718586340becdc5abe47b48682f8ef385221
Philippe Ndiaye <phndiaye@gmail.com>
2023-02-23 20:10:41 +0100

    Migrate to Yarn 3

--- packages/test-app/ember-cli-build.js
+++ packages/test-app/ember-cli-build.js
@@ -4,7 +4,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
     autoImport: {
       watchDependencies: ['@upfluence/hyperevents']
     }
