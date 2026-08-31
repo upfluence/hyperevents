ee6875177a62c7b8994f27a7c7201c5eaaaa7336
Edouard Misset <edouardmisset@gmail.com>
2026-08-31 12:28:00 +0200

    chore: configure shared eslint

---
+++ packages/hyperevents/eslint.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+import { defineConfig } from 'eslint/config';
+import { buildConfiguration } from '@upfluence/w-conf/eslint';
+
+export default defineConfig(
+  ...buildConfiguration(
+    {
+      ignores: [
+        'blueprints/*/files/',
+        'vendor/',
+        'dist/',
+        'tmp/',
+        'node_modules/',
+        'coverage/',
+        'declarations/',
+        '.node_modules.ember-try/',
+        'bower.json.ember-try',
+        'package.json.ember-try',
+        'package-lock.json.ember-try',
+        '.eslintcache'
+      ],
+      nodeFiles: ['addon-main.cjs', 'babel.config.cjs', '.template-lintrc.js']
+    },
+    {
+      files: ['rollup.config.mjs'],
+      languageOptions: {
+        globals: {
+          process: 'readonly'
+        }
+      }
+    }
+  )
+);
